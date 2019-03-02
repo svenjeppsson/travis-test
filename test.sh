9eed0917b8ea383fbf265056d43c6c1ff10c0a5b
Sven Jeppsson <sven@jeppsson.de>
2019-03-02 11:25:05 +0100

    hopefully fixed testrunner

--- test.sh
+++ test.sh
@@ -20,5 +20,5 @@ function waitContainer {
 docker run --rm -v $(pwd)/schema.sql:/docker-entrypoint-initdb.d/schema.sql --name testdb -e MYSQL_ROOT_PASSWORD=secret --health-cmd='mysqladmin ping --silent' -d mariadb:10
 waitContainer testdb
 docker build . -t go-alltest
-docker run --rm --link testdb:mysql --link sonarqube:sonarqube -v "`pwd`/.cache/pkg:/go/pkg" -v "`pwd`:/go/src/app"  go-alltest
+docker run --rm --link testdb:mysql -v "`pwd`/.cache/pkg:/go/pkg" -v "`pwd`:/go/src/app"  go-alltest
 docker stop testdb
