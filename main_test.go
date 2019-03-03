a2330360574405e3ffe673c3cf5d115332aa333b
Sven Jeppsson <sven@jeppsson.de>
2019-03-03 07:08:33 +0100

    add some comments

--- main_test.go
+++ main_test.go
@@ -19,19 +19,32 @@ func TestIntegration(t *testing.T) {
 
 }
 
+//You can make every method mockable by creating a func
+//In main.go we do
+//	var Println = fmt.Println
+//In the the testmethod we can now moch Println
+//	var test string
+//	Println = func(a ...interface{}) (n int, err error) {
+//		test = fmt.Sprintf("%v",a[0])
+//		return 0, nil
+//	}
 func TestApp_main(t *testing.T) {
 	var test string
 	Println = func(a ...interface{}) (n int, err error) {
-		test = fmt.Sprint(a)
+		test = fmt.Sprintf("%v", a[0])
 		return 0, nil
 	}
 	os.Setenv("TESTVAR", "testvalue")
 	main()
 	if test != "testvalue" {
-		t.Error("main sould use TESTVAR")
+		t.Errorf("main should print the content of the env var TESTVAR: '%v' but it prints '%v' ", os.Getenv("TESTVAR"), test)
 	}
 }
 
+//To Test the error handling you can force a mocked Method to return an error
+//	sqlOpen = func(driverName, dataSourceName string) (db *sql.DB, e error) {
+//		return nil, errors.New("Failed")
+//	}
 func TestApp_Initialize(t *testing.T) {
 	sqlOpen = func(driverName, dataSourceName string) (db *sql.DB, e error) {
 		return nil, errors.New("Failed")
